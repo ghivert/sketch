9c6ef89abdd66438d15ac2adc827d52bdca67358
Guillaume Hivert <hivert.is.coming@gmail.com>
2024-04-05 02:47:59 +0200

    refactor: improve computeProperties readability
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- src/craft.ffi.mjs
+++ src/craft.ffi.mjs
@@ -1,88 +1,89 @@
-import * as gleam from './gleam.mjs'
 import { cache } from './cache.ffi.mjs'
 import * as helpers from './helpers.ffi.mjs'
+
+function determineStyleType(style) {
+  if ('class_name' in style && typeof style.class_name === 'string')
+    return 'compose'
+  if ('key' in style && 'value' in style)
+    return 'property'
+  if ('pseudo_selector' in style && 'styles' in style)
+    return 'pseudoSelector'
+  if ('query' in style && 'styles' in style)
+    return 'mediaQuery'
+}
+
+function wrapClass(id, properties, indent, pseudo = '') {
+  const baseIndent = helpers.indent(indent)
+  return [`${baseIndent}.${id}${pseudo} {`, ...properties, `${baseIndent}}`].join('\n')
+}
+
+function computeProperty(indent, property) {
+  const baseIndent = helpers.indent(indent)
+  const key = `${baseIndent}${property.key}`
+  const important = property.important ? ' !important' : ''
+  return `${key}: ${property.value}${important};`
+}
 
 function computeProperties(rawProperties, indent = 2) {
   const properties = rawProperties.toArray()
   const init = { properties: [], medias: [], classes: [], pseudoSelectors: [], indent }
-  return properties.reduce((acc, property) => {
-    const { properties, medias, classes, pseudoSelectors, indent } = acc
-    const baseIndent = helpers.indent(indent)
-    if ('class_name' in property && typeof property.class_name === 'string') {
-      return { properties, medias, classes: [...classes, property.class_name], pseudoSelectors, indent }
-    }
-    if ('key' in property && 'value' in property) {
-      const cssProperty = `${baseIndent}${property.key}: ${property.value}${property.important ? ' !important' : ''};`
-      return { properties: [...properties, cssProperty], medias, classes, pseudoSelectors, indent }
-    }
-    if ('pseudo_selector' in property && 'styles' in property) {
-      const { pseudo_selector, styles } = property
-      const computedProperties = computeProperties(styles, 4)
-      return {
-        properties,
-        medias,
-        classes,
-        pseudoSelectors: [
-          ...pseudoSelectors,
+  return properties.reduce((accumulator, property) => {
+    switch (determineStyleType(property)) {
+      case 'compose': {
+        const classes = [...accumulator.classes, property.class_name]
+        return { ...accumulator, classes }
+      }
+      case 'property': {
+        const cssProperty = computeProperty(accumulator.indent, property)
+        const properties = [...accumulator.properties, cssProperty]
+        return { ...accumulator, properties }
+      }
+      case 'pseudoSelector': {
+        const { pseudo_selector, styles } = property
+        const computedProperties = computeProperties(styles, 4)
+        const pseudoSelector = { properties: computedProperties.properties, pseudoSelector: pseudo_selector }
+        const pseudoSelectors = [
+          ...accumulator.pseudoSelectors,
           ...computedProperties.pseudoSelectors,
-          {
-            properties: computedProperties.properties,
-            pseudoSelector: pseudo_selector,
-          },
-        ],
+          pseudoSelector,
+        ]
+        return { ...accumulator, pseudoSelectors }
       }
-    }
-    if ('query' in property && 'styles' in property) {
-      const { query, styles } = property
-      const computedProperties = computeProperties(styles, 4)
-      return {
-        properties,
-        medias: [
-          ...medias,
-          {
-            query,
-            properties: computedProperties.properties,
-            pseudoSelectors: computedProperties.pseudoSelectors,
-          },
-        ],
-        pseudoSelectors,
-        classes,
-        indent,
+      case 'mediaQuery': {
+        const { query, styles } = property
+        const { properties, pseudoSelectors } = computeProperties(styles, 4)
+        const media = { query, properties, pseudoSelectors }
+        const medias = [...accumulator.medias, media]
+        return { ...accumulator, medias }
       }
+      default:
+        return accumulator
     }
-    return { properties, medias, classes, pseudoSelectors, indent }
   }, init)
+}
+
+function computeClasses(id, computedProperties) {
+  const { properties, medias, classes, pseudoSelectors } = computedProperties
+  const classDef = wrapClass(id, properties, 0)
+  const mediasDef = medias.map(({ query, properties, pseudoSelectors }) => {
+    const sels = pseudoSelectors.map(p => wrapClass(id, p.properties, 2, p.pseudoSelector))
+    return [`${query} {`, wrapClass(id, properties, 2), ...sels, '}'].join('\n')
+  })
+  const selectorsDef = pseudoSelectors.map(p => wrapClass(id, p.properties, 0, p.pseudoSelector))
+  const name = `${classes.join(' ')} ${id}`.trim()
+  return { classDef, mediasDef, selectorsDef, name }
 }
 
 export function compileClass(args, classId) {
   const className = classId ?? helpers.getFunctionName()
   const content = cache.persist(className)
   if (content) return content
-
   // Keeping track of test to better display class names.
   // const id = classId?.replace(/[ ,#\.()]/g, '-') ?? uniqueId()
   const id = helpers.uid(className)
-  const { properties, medias, classes, pseudoSelectors } = computeProperties(args)
-  const wrapClass = (properties, indent, pseudo = '') => {
-    const baseIndent = helpers.indent(indent)
-    return [`${baseIndent}.${id}${pseudo} {`, ...properties, `${baseIndent}}`].join('\n')
-  }
-  const classDef = wrapClass(properties, 0)
-  const mediasDef = medias.map(({ query, properties, pseudoSelectors }) => {
-    const rule = [`${query} {`, wrapClass(properties, 2)].join('\n')
-    const sels = pseudoSelectors.map(({ properties, pseudoSelector }) => wrapClass(properties, 2, pseudoSelector))
-    return [rule, ...sels, '}'].join('\n')
-  })
-  const selectorsDef = pseudoSelectors.map(({ properties, pseudoSelector }) => wrapClass(properties, 0, pseudoSelector))
-  const name = `${classes.join(' ')} ${id}`.trim()
-
-  cache.store(className, {
-    name,
-    previousArgs: args,
-    indexRules: null,
-    definitions: { mediasDef, selectorsDef, classDef }
-  })
-
+  const computedProperties = computeProperties(args)
+  const { name, ...definitions } = computeClasses(id, computedProperties)
+  cache.store(className, { name, definitions, previousArgs: args, indexRules: null })
   return { name, className }
 }
 
